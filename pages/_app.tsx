f5bac00b5a717aa1a71dd04aaa6cc8eb174af355
Eric Brearley <eric.r.brearley@gmail.com>
2021-07-31 19:49:20 +1000

    Add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import React from 'react';
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 export default MyApp
